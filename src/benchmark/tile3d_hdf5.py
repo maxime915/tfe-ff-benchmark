ee940bfb9f7580a87b3d02e03b3fa0386733f16d
Maxime Amodei <maxime.amodei@gmail.com>
2021-10-05 20:03:48 +0200

    added 3d slice benchmark for zarr and hdf5

---
+++ src/benchmark/tile3d_hdf5.py
@@ -0,0 +1,89 @@
+"tile3d_zarr: benchmark zarr file by loading a 3D tile"
+
+import os
+import random
+import sys
+import typing
+
+import h5py
+
+from .benchmark_abc import BenchmarkABC
+from .utils import basic_parser
+
+
+class Tile3DHDF5Benchmark(BenchmarkABC):
+    "access an HDF5 dataset with a random 3D slice"
+
+    def __init__(self, path: str, tile: typing.List[int]) -> None:
+        self.path = path
+        self.tile = tile
+
+    def task(self) -> None:
+        file = h5py.File(self.path, mode='r')
+        array = file['data']
+
+        # re-compute slice based on array shape
+        tile = (t if t > 0 else s // -t for (s, t)
+                in zip(array.shape, self.tile))
+
+        # top left corner of the tile
+        point = (random.randrange(s - t) if s !=
+                 t else 0 for (s, t) in zip(array.shape, tile))
+
+        # get data
+        slice_lst = tuple(slice(p, p+t) for (p, t) in zip(point, tile))
+        data = array[slice_lst]
+
+        # simulate action
+        data.sum()
+        file.close()
+
+    def info(self, _) -> str:
+        file = h5py.File(self.path, mode='r')
+        data = file['data']
+
+        info = f'HDF5 Companion: {self.file}'
+        info += (f'\n\tshape = {data.shape}, chunks = {data.chunks}, '
+                 f'compression = {data.compression}')
+
+        file.close()
+        return info
+
+
+def run_benchmark(files: typing.List[str], number: int, repeat: int,
+                  tile: typing.Union[int, typing.Iterable[int]] = (32,), enable_gc: bool = False) -> None:
+
+    for file in files:
+        Tile3DHDF5Benchmark(file, tile).bench(
+            number=number, repeat=repeat, enable_gc=enable_gc)
+
+
+def _main():
+    parser = basic_parser()
+
+    parser.add_argument('--tile', nargs='+', default=[128, 128, 128], type=int,
+                        help="size of the tile to load. if tile[i] < 0 then shape[i] // (-tile[i]) is used.")
+
+    args = parser.parse_args()
+
+    tile = args.tile
+    if len(tile) == 1:
+        tile = 3 * tile
+    elif len(tile) > 3:
+        sys.exit(
+            f'invalid tile size {tile}, must not contain more than 3 values')
+    elif len(tile) == 2:
+        tile.append(1)
+
+    for t in tile:
+        if t == 0:
+            sys.exit(f'invalid tile size (0) in {tile}')
+
+    run_benchmark(args.files, args.number, args.repeat, tile, args.gc)
+    for file in args.files:
+        Tile3DHDF5Benchmark(file, tile).bench(
+            number=args.number, repeat=args.repeat)
+
+
+if __name__ == '__main__':
+    _main()
---
+++ src/benchmark/tile3d_zarr.py
@@ -0,0 +1,92 @@
+"tile3d_zarr: benchmark zarr file by loading a 3D tile"
+
+import os
+import random
+import sys
+import typing
+
+import zarr
+
+from .benchmark_abc import BenchmarkABC
+from .utils import basic_parser
+
+
+class Tile3DZarrBenchmark(BenchmarkABC):
+    "access a Zarr array with a random 3D slice"
+
+    def __init__(self, path: str, tile: typing.List[int]) -> None:
+        self.path = path
+        self.tile = tile
+
+    def task(self) -> None:
+        array = zarr.open_group(self.path, mode='r')[0]
+
+        # re-compute slice based on array shape
+        tile = (t if t > 0 else s // -t for (s, t)
+                in zip(array.shape, self.tile))
+
+        # top left corner of the tile
+        point = (random.randrange(s - t) if s !=
+                 t else 0 for (s, t) in zip(array.shape, tile))
+
+        # get data
+        slice_lst = tuple(slice(p, p+t) for (p, t) in zip(point, tile))
+        data = array[slice_lst]
+
+        # simulate action
+        data.sum()
+
+    def info(self, _) -> str:
+        data = zarr.open(self.path, mode='r')['0']
+
+        info = f'Zarr file: {self.path}'
+
+        zarr_info = str(data.info)[:-1].split('\n')
+        zarr_info = [item.split(':') for item in zarr_info]
+        zarr_info = {l.strip(): r.strip() for (l, r) in zarr_info}
+
+        info += (f'\n\tshape = {data.shape}, chunks = {data.chunks}, '
+                 f'compressor = {data.compressor}, filters = {data.filters}')
+        info += (f'\n\torder = {data.order}, size (mem) = {zarr_info["No. bytes"]}'
+                 f', size (disk) {zarr_info["No. bytes stored"]}')
+
+        return info
+
+
+def run_benchmark(files: typing.List[str], number: int, repeat: int,
+                  tile: typing.Union[int, typing.Iterable[int]] = (32,), enable_gc: bool = False) -> None:
+
+    for file in files:
+        Tile3DZarrBenchmark(file, tile).bench(
+            number=number, repeat=repeat, enable_gc=enable_gc)
+
+
+def _main():
+    parser = basic_parser()
+
+    parser.add_argument('--tile', nargs='+', default=[128, 128, 128], type=int,
+                        help="size of the tile to load. if tile[i] < 0 then shape[i] // (-tile[i]) is used.")
+
+    args = parser.parse_args()
+
+    tile = args.tile
+    if len(tile) == 1:
+        tile = 3 * tile
+    elif len(tile) > 3:
+        sys.exit(
+            f'invalid tile size {tile}, must not contain more than 3 values')
+    elif len(tile) == 2:
+        tile.append(1)
+
+    for t in tile:
+        if t == 0:
+            sys.exit(f'invalid tile size (0) in {tile}')
+
+    run_benchmark(args.files, args.number, args.repeat, tile, args.gc)
+    for file in args.files:
+        Tile3DZarrBenchmark(file, tile).bench(
+            number=args.number, repeat=args.repeat)
+
+
+if __name__ == '__main__':
+    _main()
