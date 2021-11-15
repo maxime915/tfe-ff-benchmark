53d7dfdf66f5d5b16ed7919b246203308c51d611
Maxime Amodei <maxime.amodei@gmail.com>
2021-11-15 11:06:54 +0100

    added db inspect tool

---
+++ src/meetings/nov23/inspect_db.py
@@ -0,0 +1,28 @@
+"inspect_db: show information on a result file"
+
+import argparse
+
+from .db import DB
+
+
+def inspect(db_path: str, full: bool = False) -> None:
+    ""
+    db = DB(db_path)
+    content = db.load_all()
+    for key, value in content.items():
+        if full:
+            print(f'db[{key}] = {value}')
+        else:
+            print(key)
+
+
+if __name__ == "__main__":
+    _parser = argparse.ArgumentParser()
+    _parser.add_argument('files', type=str, nargs='+')
+    _parser.add_argument('--full', action='store_true', dest='full')
+    _parser.set_defaults(full=False)
+
+    _args = _parser.parse_args()
+
+    for _file in _args.files:
+        inspect(_file, _args.full)
