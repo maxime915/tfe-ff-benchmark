628492eb5fb2ab4c0eceb3dd6e4b0f5c171d04a9
Maxime Amodei <maxime.amodei@gmail.com>
2021-11-26 09:08:02 +0100

    pyramidal tiff benchmark with overlap (wip)

--- src/benchmark/pyramidal_tiff.py
+++ src/benchmark/pyramidal_tiff.py
@@ -7,7 +7,7 @@ import typing
 import tifffile
 
 from .benchmark_abc import BenchmarkABC, Verbosity
-from .utils import parse_args_with_tile
+from .utils import parse_args_with_tile, tiled_parser
 
 _VALID_SUFFIX_S = ('_C0_Z0_T0.tif', '_C0_Z0_T0_pyr.tif')
 
@@ -66,10 +66,107 @@ class PyramidalTiffBenchmark(BenchmarkABC, re_str=r'.*_pyr\.tif(f)?'):
         return info
 
 
+class PyramidalTiffOverlapBenchmark(PyramidalTiffBenchmark):
+    "2D benchmark but the loaded tile fit entirely inside a data chunk"
+
+    def __init__(self, file: str, tile: typing.Tuple[int, int], overlap: typing.Tuple[int, int]) -> None:
+        super().__init__(file, tile)
+        self.overlap = overlap
+
+    def task(self) -> None:
+        # Z pages of YX planes
+        tif = tifffile.TiffFile(self.file)
+
+        # get a random Z plane
+        array = random.choice(tif.pages)
+
+        # fraction tile: set -2 to size/2, etc
+        tile = [t if t > 0 else s // -t for (s, t)
+                in zip(array.shape, self.tile)]
+        # array.shape[i] >= tile[i] > 0
+        # we can suppose tile[i] <= chunk[i]
+
+        print(f'{array.shape = }')
+        print(f'{array.chunks = }')
+        print(f'{tile = }')
+
+        chunk_shape = array.chunks
+        # array.shape[i] >= chunk_shape[i] > 0
+
+        # how many (full) chunk per axis
+        chunk_count = [s // c for (s, c) in zip(array.shape, chunk_shape)]
+        # chunk_count[i] > 0
+
+        print(f'{chunk_count = }')
+
+        # select one random full chunk
+        chunk_idx = [random.randrange(c-o)+o if c-o !=
+                     0 else o for (c, o) in zip(chunk_count, self.overlap)]
+        # chunk_count[i] > chunk_idx[i] >= overlap[i]
+
+        print(f'{chunk_idx = }')
+
+        # if overlap
+        #   set a halfway before the chunk edge (to cross it)
+        # else
+        #   select a brand new point
+        point = [i * c - t // 2 if o else i * c for (i, c, o, t)
+                 in zip(chunk_idx, chunk_shape, self.overlap, tile)]
+        # array.shape[i] > point[i] + tile[i]
+
+        print(f'{point = }')
+
+        # get a tuple slice objects as index
+        slice_tpl = tuple(slice(p, p+t) for (p, t) in zip(point, tile))
+
+        print(f'{slice_tpl = }')
+
+        array = array.asarray()
+        print(f'{array.shape}')
+
+        # get data & simulate action
+        data = array.asarray()[slice_tpl]
+        data.sum()
+
+        tif.close()
+
+    def info(self, verbosity: Verbosity) -> str:
+        tif = tifffile.TiffFile(self.file)
+        page = tif.pages[0]
+
+        info = f'OME-Tiff: {self.file}, tile: {self.tile}, overlap: {self.overlap}'
+
+        if verbosity == verbosity.VERBOSE:
+            info += (f'\n\tshape = {len(tif.pages)} * {page.shape}, '
+                     f'chunks = {page.chunks}, chunked = {page.chunked}')
+
+        tif.close()
+        return info
+
+
 def _main():
-    args = parse_args_with_tile(ndim=2)
+    parser = tiled_parser(ndim=2)
+
+    overlap = parser.add_mutually_exclusive_group()
+    overlap.add_argument('--random-overlap', action='store_const', const=None, dest='overlap',
+                         help='randomly select a tile, in or out a chunk')
+    overlap.add_argument('--no-overlap', action='store_const', const=(0, 0), dest='overlap',
+                         help='always select a tile fully inside a chunk')
+    overlap.add_argument('--overlap-x', action='store_const', const=(1, 0), dest='overlap',
+                         help='always select a tile that overlap on the first axis only')
+    overlap.add_argument('--overlap-y', action='store_const', const=(0, 1), dest='overlap',
+                         help='always select a tile that overlap on the second axis only')
+    overlap.add_argument('--overlap-xy', action='store_const', const=(1, 1), dest='overlap',
+                         help='always select a tile that overlap on both axes')
+    overlap.set_defaults(overlap=None)
+
+    args = parser.parse_args()
 
     for file in args.files:
+        if args.overlap is not None:
+            PyramidalTiffOverlapBenchmark(file, args.tile, args.overlap).bench(
+                Verbosity.VERBOSE, enable_gc=args.gc, number=args.number, repeat=args.repeat)
+            continue
         PyramidalTiffBenchmark(file, args.tile).bench(
             Verbosity.VERBOSE, enable_gc=args.gc, number=args.number, repeat=args.repeat)
 
